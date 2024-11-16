86817a27afa46052996739b5c2c96e8fb08e8dd0
naotaka nakane <n.nakane0219@gmail.com>
2024-11-16 05:25:54 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,14 @@
 set -eu
 
 ln -sfn "$(pwd)/.gitconfig" ~/.gitconfig
+ln -sfn "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -sfn "$(pwd)/.zshenv" ~/.zshenv
 ln -sfn "$(pwd)/.zshrc" ~/.zshrc
-ln -sfn "$(pwd)/.tmux.conf" ~/.tmux.conf
 mkdir -p ~/.config/nvim
 ln -sfn "$(pwd)/init.lua" ~/.config/nvim/init.lua
 ln -sfn "$(pwd)/lua" ~/.config/nvim/lua
 ln -sfn "$(pwd)/luasnip" ~/.config/nvim/luasnip
-mkdir -p ~/.config/git
-ln -sfn "$(pwd)/.config/git/ignore" ~/.config/git/ignore
+ln -sfn "$(pwd)/.config/git" ~/.config/git
 ln -sfn "$(pwd)/.config/starship.toml" ~/.config/starship.toml
-
-ln -sfn "$(pwd)/.config/zsh" ~/.config/zsh
-
-# for local
 ln -sfn "$(pwd)/.config/wezterm" ~/.config/wezterm
+ln -sfn "$(pwd)/.config/zsh" ~/.config/zsh
