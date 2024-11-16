bb09abb801ef9e8b4ef957bc0c85f81437e0f875
naotaka nakane <n.nakane0219@gmail.com>
2024-11-16 05:41:32 +0000

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ ln -sfn "$(pwd)/.gitconfig" ~/.gitconfig
 ln -sfn "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -sfn "$(pwd)/.zshenv" ~/.zshenv
 ln -sfn "$(pwd)/.zshrc" ~/.zshrc
+mkdir -p ~/.config
 ln -sfn "$(pwd)/.config/git" ~/.config/git
 ln -sfn "$(pwd)/.config/nvim" ~/.config/nvim
 ln -sfn "$(pwd)/.config/starship.toml" ~/.config/starship.toml
